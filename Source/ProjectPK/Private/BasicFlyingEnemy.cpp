d9dfcf5bb49fe65668fe8c434fbd3fa241308702
Zablas <vagabaliukas@gmail.com>
2020-03-10 23:45:03 +0200

    Improvements on BasicFlyingEnemy
    Basic flying enemy now returns to its correct Z coordinate using interpolation. Timer, detect range and interpolation speed need tweaking for a smooth experience. Some other mechanics were also polished.

--- Source/ProjectPK/Private/BasicFlyingEnemy.cpp
+++ Source/ProjectPK/Private/BasicFlyingEnemy.cpp
@@ -4,7 +4,10 @@
 #include "BasicFlyingEnemy.h"
 #include "Components/SphereComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Engine/World.h"
 #include "GameFramework/PawnMovementComponent.h"
+#include "Math/UnrealMathUtility.h"
+#include "TimerManager.h"
 #include "MainCharacter.h"
 
 ABasicFlyingEnemy::ABasicFlyingEnemy()
@@ -16,13 +19,18 @@ ABasicFlyingEnemy::ABasicFlyingEnemy()
 
 	target = nullptr;
 	bIsMovingRight = false;
+	bIsInterpolating = false;
+	bIsTimerSet = false;
 	patrolEndX = 1200.f;
+	interpSpeed = 4.f;
+	timerDelay = 2.f;
 }
 
 void ABasicFlyingEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 	initLocation = GetActorLocation();
+	locationBeforeChase = initLocation;
 	detectRange->OnComponentBeginOverlap.AddDynamic(this, &ABasicFlyingEnemy::OnOverlapBegin);
 	detectRange->OnComponentEndOverlap.AddDynamic(this, &ABasicFlyingEnemy::OnOverlapEnd);
 }
@@ -30,16 +38,17 @@ void ABasicFlyingEnemy::BeginPlay()
 void ABasicFlyingEnemy::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
-	MoveToTarget();
+	MoveToTarget(DeltaSeconds);
 }
 
 void ABasicFlyingEnemy::OnOverlapBegin(UPrimitiveComponent* OverlappedComponentclass, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor && OtherActor != this) //Uztikrinam, kad kitas aktorius validus ir ne tas pats kaip dabar.
+	if (OtherActor && OtherActor != this && !bIsInterpolating) //Uztikrinam, kad kitas aktorius validus ir ne tas pats kaip dabar.
 	{
 		AMainCharacter* player = Cast<AMainCharacter>(OtherActor);
-		if (player) //Jeipavyko pacastinti i zaidejo klase
+		if (player) //Jei pavyko pacastinti i zaidejo klase
 		{
+			locationBeforeChase = GetActorLocation();
 			target = player;
 		}
 	}
@@ -51,28 +60,56 @@ void ABasicFlyingEnemy::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, A
 	GetMovementComponent()->StopActiveMovement();
 }
 
-void ABasicFlyingEnemy::MoveToTarget()
+void ABasicFlyingEnemy::MoveToTarget(float DeltaSeconds)
 {
 	if (target) //Tik jei turim validu taikini, judesim link jo
 	{
 		FVector toNormalize = target->GetActorLocation() - this->GetActorLocation();
 		toNormalize.Normalize();
 		AddMovementInput(toNormalize);
+		if(target->GetActorLocation().X < this->GetActorLocation().X) SetActorRotation(FRotator(0.f, 180.f, 0.f));
+		else SetActorRotation(FRotator(0.f)); //Nustatom, i kuria puse ziureti priesui
 	}
 	else //Jei tokio neturim patruliuosim
 	{
-		if (bIsMovingRight) //I desine
+		FVector currentLocation = GetActorLocation();
+		if (!FMath::IsNearlyEqual(currentLocation.Z, locationBeforeChase.Z, 1.f)) //Jei nesam tam paciam auksty
+		{
+			bIsInterpolating = true;
+			FVector interpVec = FMath::VInterpTo(currentLocation, locationBeforeChase, DeltaSeconds, interpSpeed);
+			SetActorLocation(interpVec);
+			UE_LOG(LogTemp, Warning, TEXT("Interpoliuojamas vektorius: %s"), *interpVec.ToString());
+		}
+		else if (bIsInterpolating && !bIsTimerSet) //Pasiruosiam pazymeti, kad nevyksta interpoliacija
+		{
+			bIsTimerSet = true;
+			GetWorld()->GetTimerManager().SetTimer(negationTimer, this, &ABasicFlyingEnemy::NegateInterpolation, timerDelay, false);
+		}
+
+		//Vyks tik jei neinterpoliuojam i buvusia vieta
+		if (bIsMovingRight && !bIsInterpolating) //I desine
 		{
 			AddMovementInput(FVector(1.f, 0.f, 0.f));
-			FVector deltaVec = GetActorLocation() - initLocation;
+			FVector deltaVec = currentLocation - initLocation;
 			if (deltaVec.X >= patrolEndX) bIsMovingRight = false;
+			SetActorRotation(FRotator(0.f));
+			UE_LOG(LogTemp, Warning, TEXT("Juda i desine"));
 		}
-		else //I kaire
+		else if (!bIsMovingRight && !bIsInterpolating) //I kaire
 		{
 			AddMovementInput(FVector(-1.f, 0.f, 0.f));
-			FVector deltaVec = GetActorLocation() - initLocation;
+			FVector deltaVec = currentLocation - initLocation;
 			if (deltaVec.X <= 0) bIsMovingRight = true;
+			SetActorRotation(FRotator(0.f, 180.f, 0.f));
+			UE_LOG(LogTemp, Warning, TEXT("Juda i kaire"));
 		}
 	}
+}
+
+void ABasicFlyingEnemy::NegateInterpolation()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Pakviestas delegtatas"));
+	bIsInterpolating = false;
+	bIsTimerSet = false;
 }
 
--- Source/ProjectPK/Public/BasicFlyingEnemy.h
+++ Source/ProjectPK/Public/BasicFlyingEnemy.h
@@ -22,14 +22,23 @@ protected:
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaSeconds) override;
 
-    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AI")
-    class USphereComponent* detectRange;
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CustomAI")
+    class USphereComponent* detectRange; //Sfera, pagal kuria nustatomas zaidejo aptikimas
 
-    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "AI | Combat")
-    class AMainCharacter* target;
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "CustomAI | Combat")
+    class AMainCharacter* target; //Veikejas, kuri vejames
 
-    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "AI")
-    bool bIsMovingRight;
+    UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "CustomAI | Movemement")
+    bool bIsMovingRight; //Nustato krypti judeti i kaire ar desine
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CustomAI | Movemement")
+    float patrolEndX; //Ribos X asyje, kiek judeti pasyviai pirmyn atgal
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CustomAI | Movemement")
+    float interpSpeed; //Interpoliacijos greitis
+
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CustomAI | Movemement")
+    float timerDelay; //Uz kiek laiko paleisti negacijos timeri
 
 public:
     UFUNCTION()
@@ -39,9 +48,15 @@ public:
     void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
     UFUNCTION()
-    void MoveToTarget();
+    void MoveToTarget(float DeltaSeconds);
+
+    UFUNCTION()
+    void NegateInterpolation(); //Skirtas nufalseinti interpoliacijos booli
 
 private:
-    FVector initLocation;
-    float patrolEndX;
+    FVector initLocation; //Pradine vieta, jam atsispawninus
+    FVector locationBeforeChase; //Vieta pries pradedant vytis zaideja
+    bool bIsInterpolating; //Ar interpoliuoja
+    bool bIsTimerSet; //Ar timeris uzstatytas (kad amzinai nenusiresetintu)
+    FTimerHandle negationTimer; //I ka kaupiam timerio rezultatus
 };
